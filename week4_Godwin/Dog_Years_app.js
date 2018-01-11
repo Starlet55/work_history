2d1e8496f430152cde5ea6d35e25394df479993e
Starlet55 <godwinuche2@gmail.com>
2018-01-11 22:16:26 +0100

    update my dog years work on mathematical assignment operators

---
+++ week4_Godwin/Dog_Years_app.js
@@ -0,0 +1,9 @@
+const myAge = 24; // my current age
+let earlyYears = 2; // computation for dog years
+earlyYears *= 10.5;
+let laterYears = myAge - 2; // subtract the earlyYears
+laterYears *= 4; // 88 years later
+myAgeInDogYears = earlyYears + laterYears; //my age in dog years
+const myName = 'Uchenna';
+('myName'.toLowerCase()); // change my name to lowercase
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`) // my age in dog years
