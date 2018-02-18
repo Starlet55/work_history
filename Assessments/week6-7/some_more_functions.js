b4c13f37d1ff2399a3d29fe9c11da24cec3b05f4
Starlet55 <godwinuche2@gmail.com>
2018-02-19 00:44:25 +0100

    updated week6-7 Assessment

--- Assessments/week6-7/some_more_functions.js
+++ Assessments/week6-7/some_more_functions.js
@@ -20,6 +20,17 @@
 // EX1:
   // sample input: "Hello World"
   // sample output: 3
+  const vowels = ['a','e','i','o','u'];
+let output = 0;
+
+  const numberOfVowels = (vowelString) =>{
+    for (let vowelIndex = 0; vowelIndex < vowelString.length; vowelIndex++){
+      if(vowelString[vowelIndex] === vowels){
+        output++
+      }
+    }
+    return output;
+  }
 
 // 2. write a function that takes a string as it first parameter. This function
 // return a new string with the first 3 letters lower-cased. If the string is less
@@ -31,6 +42,14 @@
 // EX2:
   // sample input: "he"
   // sample output: "HE"
+  const threeLowerCaseLetters = (anyString) =>{
+    if(anyString.length >= 3){
+      return anyString.slice(0,3).toLowerCase() + anyString.slice(3).toUpperCase();
+    }else (anyString.length < 3){
+      return anyString.toUpperCase();
+    }
+
+  }
 
 // 3. write a function that takes a string as it first parameter. This function
 // returns the first half of the string if the string is even length. Otherwise,
@@ -41,6 +60,13 @@
 // EX2:
   // sample input: "Hello World"
   // sample output: null
+  const evenString = (theString) =>{
+    if(theString.length %2 === 0){
+      return theString.slice(0, theString / 2);
+    }else {
+      return null;
+    }
+  }
 
 // 4. write a function that takes an array of numbers as its first parameter.
 // This function returns the sum of the numbers in the array
@@ -48,7 +74,7 @@
   // sample input: [1, 7, 1, 4, 1, 6, 4, 5]
   // sample output: 29
 
-// 5. write a function that takes an array of numbers as its first parameter.
+// 5. write a funcstion that takes an array of numbers as its first parameter.
 // This function returns an the number that is the most frequent in the array
 // EX:
   // sample input: [1, 7, 1, 4, 1, 6, 4, 5]
